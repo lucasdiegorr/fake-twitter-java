70985dbcac1478a264b1e27b6f1482008c817981
Lucas Rocha <lucas.diegorr@gmail.com>
2022-02-11 23:21:40 -0300

    first commit

---
+++ src/main/java/com/github/lucasdiegorr/faketwitterjava/FakeTwitterJavaApplication.java
@@ -0,0 +1,13 @@
+package com.github.lucasdiegorr.faketwitterjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeTwitterJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeTwitterJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/lucasdiegorr/faketwitterjava/FakeTwitterJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lucasdiegorr.faketwitterjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeTwitterJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
